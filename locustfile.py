343626bf8fc55d6f98261e7b03de978288afc981
EdgarChang <ken2798315601@gmail.com>
2020-04-06 17:40:14 +0800

    refactor locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -21,49 +21,45 @@ class UserBehavior(TaskSet):
 		##json web token驗證
 		jsonwebtoken_response = self.client.post('/api/token/', 
 			json={'username': 'admin', 'password': 'password'}
-		)
+			)
+		
 		self.json_web_token = json.loads(jsonwebtoken_response.text)
 		self.access_token = self.json_web_token['access']
 		self.refresh_token = self.json_web_token['refresh']
+		self.JWTheaders = {'Authorization': 'Bearer ' + self.access_token}
 	
 	def use_refresh_token_get_new_access_token(self):
 		use_refresh_token_response = self.client.post('/api/token/refresh/', 
 			json={'refresh':self.refresh_token}
 			)
+		
 		refreshed_access_token = json.loads(use_refresh_token_response.text)
 		self.access_token = refreshed_access_token['access']
+		self.JWTheaders = {'Authorization': 'Bearer ' + self.access_token}
 	
-	@task
+	@task(1)
 	def event_list(self):
-		response = self.client.get('/Events/' , 
-			headers = {'Authorization': 'Bearer ' + self.access_token})
+		with self.client.get('/Events/' , headers = self.JWTheaders) as response :
+			if response.status_code == 401:
+				self.use_refresh_token_get_new_access_token()
 		
-		if response.status_code == 401:
-			self.use_refresh_token_get_new_access_token()
-			
-	@task
+	@task(0)
 	def guest_list(self):
-		response = self.client.get('/Guests/?limit=50&offset=1000' , 
-			headers = {'Authorization': 'Bearer ' + self.access_token})
-		
-		if response.status_code == 401:
-			self.use_refresh_token_get_new_access_token()
+		with self.client.get('/Guests/?limit=50&offset=1000' , headers = self.JWTheaders) as response :
+			if response.status_code == 401:
+				self.use_refresh_token_get_new_access_token()
 	
-	@task
+	@task(0)
 	def event_detail(self):
-		response = self.client.get('/Event/' + str(random.randint(1,2)) +  '/' , 
-			headers = {'Authorization': 'Bearer ' + self.access_token})
-			
-		if response.status_code == 401:
-			self.use_refresh_token_get_new_access_token()
+		with self.client.get('/Event/' + str(random.randint(1,2)) +  '/' , headers = self.JWTheaders) as response :
+			if response.status_code == 401:
+				self.use_refresh_token_get_new_access_token()
 		
-	@task
+	@task(0)
 	def guest_detail(self):
-		response = self.client.get('/Guest/7895/' , 
-			headers = {'Authorization': 'Bearer ' + self.access_token})
-			
-		if response.status_code == 401:
-			self.use_refresh_token_get_new_access_token()
+		with self.client.get('/Guest/7895/' , headers = self.JWTheaders) as response :
+			if response.status_code == 401:
+				self.use_refresh_token_get_new_access_token()
 
 class WebsiteUser(HttpLocust):
 	task_set = UserBehavior
