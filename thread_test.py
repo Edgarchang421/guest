fb2e865e81764c15123d1a2b92cb572729713082
EdgarChang <ken2798315601@gmail.com>
2020-04-08 14:37:38 +0800

    annotation thread_test.py

--- thread_test.py
+++ thread_test.py
@@ -3,9 +3,12 @@ import threading
 from time import time
 import json
 
+## 要測試的domain
 base_url = 'http://192.168.1.105:8000/'
 
+## 子執行緒的目標function
 def put_sign(start_user , end_user):
+	##先取得Json web token
 	get_token_response = requests.post(base_url + 'api/token/', 
 		json={'username': 'admin', 'password': 'password'}
 		)
@@ -14,7 +17,8 @@ def put_sign(start_user , end_user):
 	access_token = json_web_token['access']
 	refresh_token = json_web_token['refresh']
 	JWTheaders = {'Authorization': 'Bearer ' + access_token}
-		
+	
+	## 使用PUT method去update嘉賓的簽到狀態sign
 	for i in range(start_user , end_user):
 		data = {'realname' : 'edgar' + str(i),
 			'phone' : 13800110000 + i,
@@ -25,10 +29,12 @@ def put_sign(start_user , end_user):
 			
 		requests.put(base_url + 'Guest/' + str(i) + '/' , data=data , headers = JWTheaders)
 
-##建立字典，將要執行PUT的3000個id分成五組
+## 建立字典，將要執行PUT的3000個Guest id分成五組
+## 因為前面建立mysql時，刪除了兩次，所以是guest的id是6001~9000
+## range(x,y)會生成x~y-1的數字
 lists = {6001:6601 , 6601:7201 , 7201:7801 , 7801:8401 , 8401:9001}
 
-##執行緒物件的列表
+##子執行緒的列表
 threads = []
 
 ## 使用for迭代字典的 .items()
@@ -42,10 +48,11 @@ for start_user , end_user in lists.items():
 if __name__ == '__main__':
 	start_time = time()
 	
-	## 讓前面建立的五個執行緒開始執行
+	## 讓前面建立的五個子執行緒開始執行
 	for i in range(len(lists)):
 		threads[i].start()
-		
+	
+	##等待五個子執行緒都結束後，再繼續執行後面的程式
 	for i in range(len(lists)):
 		threads[i].join()
 		
